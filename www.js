0d9e9813d36d19e60368ba1359e7fdf409ab392e
SuperAuguste <auguste.rame@gmail.com>
2019-10-23 21:00:29 -0400

    fix

--- www.js
+++ www.js
@@ -26,7 +26,7 @@ function getJSON (url) {
 
 async function updatePodcastCache () {
 
-	console.log("Updating podcast page!");
+	console.log("Updating podcast page!!");
 	const json = await getJSON("https://shows.pippa.io/api/shows/5d9c8ccb34dfd91e4010ff4f/episodes?results=1000");
 	
 	if (typeof json !== "object") {
